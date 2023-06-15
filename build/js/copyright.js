5854e8bb630230bcbc5c23511e3292460f59ecbb
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-06-15 11:18:32 +0530

    Delete build directory

--- build/js/copyright.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const year = document.getElementById("year");
-const thisYear = new Date().getFullYear().toString();
-year.setAttribute("datetime", thisYear);
-year.textContent = thisYear;
